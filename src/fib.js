916ad071879ea85c360bc34f80ac4a9448b5ec56
TurniSaha <turni.saha@gmail.com>
2026-05-14 14:41:24 -0600

    feat: Create src/fib.js exporting fib(n) computing the nth Fibonacci number it
    bis-swarm task 01KRM3GRAN8A107GXFGZXJT7AS

---
+++ src/fib.js
@@ -0,0 +1,15 @@
+function fib(n) {
+  if (n < 0) throw new Error('n must be non-negative');
+  if (n < 2) return n;
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
+}
+
+module.exports = fib;
+module.exports.fib = fib;
